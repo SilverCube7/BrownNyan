ff2512792d61dd41ef14bf119df470ad6e216204
SilverCube7 <silvercube77@naver.com>
2020-05-23 14:55:38 +0900

    코드 정리

--- 화냥.js
+++ 화냥.js
@@ -16,13 +16,15 @@ const scriptName = "화냥";
  */
 
 let forbiddenWords = [];
-let db = [];
-let msgList = [];
-let msgListLimit = 100;
-let forbiddenSigns = [';', '%', '&'];
+let learnDB = [];
+let msgDB = [];
+let msgDBLimit = 300;
+let forbiddenSigns = ['{', '}'];
 
+// 냥습금지어.txt 데이터 불러오기
 function loadForbiddenWords() {
-  forbiddenWords = DataBase.getDataBase("냥습_금지어");
+  let name = "냥습금지어";
+  forbiddenWords = DataBase.getDataBase(name);
 
   if(forbiddenWords == null || forbiddenWords == "") {
     forbiddenWords = [];
@@ -32,63 +34,247 @@ function loadForbiddenWords() {
   forbiddenWords = forbiddenWords.split("{enter}");
 }
 
-function loadDB() {
-  db = DataBase.getDataBase("냥습");
+// 냥습.txt 데이터 불러오기
+function loadLearnDB() {
+  let name = "냥습";
+  learnDB = DataBase.getDataBase(name);
 
-  if(db == null || db == "") {
-    db = [];
+  if(learnDB == null || learnDB == "") {
+    learnDB = [];
     return;
   }
 
-  db = db.split("{enter}");
+  learnDB = learnDB.split("{enter}");
 
-  for(let i=0; i<db.length; i++)
-    db[i] = db[i].split("{space}");
+  for(let i=0; i<learnDB.length; i++)
+    learnDB[i] = learnDB[i].split("{space}");
 }
 
-function saveDB() {
+// 냥습.txt에 데이터 저장하기
+function saveLearnDB() {
+  let name = "냥습";
   let makeDB = "";
 
-  for(let i=0; i<db.length; i++) {
-    makeDB += db[i][0]+"{space}"+db[i][1]+"{space}"+db[i][2];
+  for(let i=0; i<learnDB.length; i++) {
+    makeDB += learnDB[i][0]+"{space}"+learnDB[i][1]+"{space}"+learnDB[i][2];
 
-    if(i != db.length-1)
+    if(i != learnDB.length-1)
       makeDB += "{enter}";
   }
 
-  DataBase.setDataBase("냥습", makeDB);
+  DataBase.setDataBase(name, makeDB);
 }
 
-function loadMsgList() {
-  msgList = DataBase.getDataBase("메시지_리스트");
+// 메시지.txt 데이터 불러오기
+function loadMsgDB() {
+  let name = "메시지";
+  msgDB = DataBase.getDataBase(name);
 
-  if(msgList == null || msgList == "") {
-    msgList = [];
+  if(msgDB == null || msgDB == "") {
+    msgDB = [];
     return;
   }
 
-  msgList = msgList.split("{enter}");
+  msgDB = msgDB.split("{enter}");
 
-  for(let i=0; i<msgList.length; i++)
-    msgList[i] = msgList[i].split("{space}");
+  for(let i=0; i<msgDB.length; i++)
+    msgDB[i] = msgDB[i].split("{space}");
 }
 
-function saveMsgList() {
-  let makeMsgList = "";
+// 메시지.txt에 데이터 저장하기
+function saveMsgDB() {
+  let name = "메시지";
+  let makeMsgDB = "";
 
-  for(let i=0; i<msgList.length; i++) {
-    makeMsgList += msgList[i][0]+"{space}"+msgList[i][1];
+  for(let i=0; i<msgDB.length; i++) {
+    makeMsgDB += msgDB[i][0]+"{space}"+msgDB[i][1];
 
-    if(i != msgList.length-1)
-      makeMsgList += "{enter}";
+    if(i != msgDB.length-1)
+      makeMsgDB += "{enter}";
   }
 
-  DataBase.setDataBase("메시지_리스트", makeMsgList);
+  DataBase.setDataBase(name, makeMsgDB);
 }
 
+/**
+ * 명령어: 냥습/A/B
+ * 설명: 메시지 A가 왔을 때 메시지 B를 보내도록 학습시킨다.
+ */
+function learn(query, sender) {
+  let A = query[1], B = query[2];
+  let forbad = false;
+
+  // A가 금지어인지 확인
+  for(let i=0; i<forbiddenWords.length; i++) {
+    if(A == forbiddenWords[i]) {
+      forbad = true;
+      break;
+    }
+  }
+
+  // A 안에 금지기호가 있는지 확인
+  for(let i=0; i<forbiddenSigns.length; i++) {
+    if(A.indexOf(forbiddenSigns[i]) != -1 || B.indexOf(forbiddenSigns[i]) != -1) {
+      forbad = true;
+      break;
+    }
+  }
+
+  // A의 맨 앞 문자 or 맨 뒤 문자가 공백인지 확인
+  if(A[0] == '\r') forbad = true;
+  if(A[0] == '\n') forbad = true;
+  if(A[0] == ' ') forbad = true;
+  if(A[A.length-1] == '\r') forbad = true;
+  if(A[A.length-1] == '\n') forbad = true;
+  if(A[A.length-1] == ' ') forbad = true;
+
+  // 너무 짧은 경우
+  if(A.length <= 1 || B.length == 0)
+    return "너무 짧다냥!";
+  
+  // A가 금지어 or A 안에 금지기호가 있는 경우
+  if(forbad)
+    return "냥습할 수 없다냥!";
+
+  let learned = false;
+
+  for(let i=0; i<learnDB.length; i++) {
+    // 학습한게 있으면 learnDB에 학습데이터 덮어쓰기
+    if(A == learnDB[i][0]) {
+      learnDB[i][1] = B;
+      learnDB[i][2] = sender;
+      learned = true;
+      break;
+    }
+  }
+
+  // 학습한게 없으면 learnDB에 학습데이터 추가
+  if(!learned)
+    learnDB.push([A, B, sender]);
+
+  saveLearnDB();
+  return "냥!";
+}
+
+/**
+ * 명령어: 냥습/A
+ * 설명: 메시지 A를 학습시켰는지 확인한다.
+ */
+function confirmLearn(query) {
+  let A = query[1];
+
+  for(let i=0; i<learnDB.length; i++) {
+    // 학습한게 있으면 학습데이터 출력
+    if(A == learnDB[i][0])
+      return learnDB[i][1]+", "+learnDB[i][2]+"님이 냥습시켯다냥!";
+  }
+
+  // 학습한게 없으면 없다고 출력
+  return "냥습한게 없다냥!";
+}
+
+/**
+ * 명령어: 삭제/A
+ * 설명: 학습되어 있는 메시지 A를 삭제한다.
+ */
+function del(query) {
+  let A = query[1];
+
+  for(let i=0; i<learnDB.length; i++) {
+    // 학습한게 있으면 삭제
+    if(A == learnDB[i][0]) {
+      learnDB.splice(i, 1);
+      saveLearnDB();
+      return "냥!";
+    }
+  }
+
+  // 학습한게 없으면 아무것도 안함
+  return "냥습한게 없다냥!";
+}
+
+/**
+ * 명령어: 말/A
+ * 조건: A는 정수
+ * 설명: 이전에 왔던 메시지를 본다.
+ */
+function prevMsg(query) {
+  let A = Number(query[1]);
+
+  // 정수가 아닌 경우
+  if(!Number.isInteger(A))
+    return "정수가 아니다냥!";
+
+  // 수가 해당 범위 안에 있지 않은 경우
+  if(!(0 <= A && msgDB.length-(A+1) >= 0))
+    return "수가 범위를 초과했다냥!";
+
+  // 이전 메시지 출력
+  return msgDB[msgDB.length-(A+1)][0]+", "+msgDB[msgDB.length-(A+1)][1]+"님이다냥!";
+}
+
+/**
+ * 명령어: 화냥폰/A
+ * 설명: 화냥폰의 정보를 본다.
+ *   A ∈ { 배터리, 전압 }
+ */
+function phone(query) {
+  let A = query[1];
+
+  // 배터리 출력
+  if(A == "배터리")
+    return Device.getBatteryLevel()+"% 남았다냥!";
+
+  // 전압 출력
+  if(A == "전압")
+    return Device.getBatteryVoltage()+"mV 이다냥!";
+
+  // 엉뚱한 정보를 출력하라고 했을 경우
+  return "이 정보는 1급기밀이다냥!";
+}
+
+/**
+ * 명령어: 냥습목록
+ * 설명: 학습 목록을 본다.
+ */
+function learnList() {
+  let list = "< 냥습목록 >\n";
+
+  for(let i=0; i<learnDB.length; i++)
+    list += String(i+1)+": "+String(learnDB[i])+'\n';
+
+  return list;
+}
+
+/**
+ * 명령어: 오늘은
+ * 설명: 오늘 날짜를 본다.
+ */
+function today() {
+  let now = new Date();
+  return now.getFullYear()+"년 "+(now.getMonth()+1)+"월 "+now.getDate()+"일이다냥!";
+}
+
+/**
+ * 명령어: 안녕 or 안녕하세요
+ */
+function hello(sender) {
+  return sender+"님 환영한다냥!";
+}
+
+/**
+ * 명령어: 화냥봇
+ */
+function nyanBot() {
+  let ans = ["냥?", "냐앙?", "왜 불럿냥?", "무슨 일이냥?"];
+  let r = Math.floor(Math.random()*ans.length);
+  return ans[r];
+}
+
+// Database에 있는 데이터 불러오기
 loadForbiddenWords();
-loadDB();
-loadMsgList();
+loadLearnDB();
+loadMsgDB();
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
   /**
@@ -97,193 +283,40 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
    *   room의 접두사가 WN
    */
   if(room == "실큡" || room.substring(0, 2) == "WN") {
-    // 메시지 리스트에 msg와 sender 추가
-    msgList.push([msg, sender]);
-    while(msgList.length > msgListLimit) msgList.shift();
-    saveMsgList();
+    // msgDB에 msg와 sender 추가
+    msgDB.push([msg, sender]);
+    while(msgDB.length > msgDBLimit) msgDB.shift();
+    saveMsgDB();
 
     let query = msg.split('/');
 
     if(query[0] == "냥습") {
-      if(query.length >= 3) {
-        /**
-         * 명령어: 냥습/A/B
-         * 설명: 메시지 A가 왔을 때 메시지 B를 보내도록 학습시킨다.
-         */
-
-        let A = query[1], B = query[2];
-        let forbad = false;
-
-        // 금지어 탐색
-        for(let i=0; i<forbiddenWords.length; i++) {
-          if(A == forbiddenWords[i]) {
-            forbad = true;
-            break;
-          }
-        }
-
-        // A의 부분문자열에서 금지기호 탐색
-        for(let i=0; i<forbiddenSigns.length; i++) {
-          if(A.indexOf(forbiddenSigns[i]) != -1 || B.indexOf(forbiddenSigns[i]) != -1) {
-            forbad = true;
-            break;
-          }
-        }
-
-        if(A[0] == '\r') forbad = true;
-        if(A[0] == '\n') forbad = true;
-        if(A[0] == ' ') forbad = true;
-        if(A[A.length-1] == '\r') forbad = true;
-        if(A[A.length-1] == '\n') forbad = true;
-        if(A[A.length-1] == ' ') forbad = true;
-
-        if(A.length <= 1 || B.length == 0) {
-          replier.reply("너무 짧다냥!");
-        } else if(forbad) {
-          replier.reply("냥습할 수 없다냥!");
-        } else {
-          let learned = false;
-
-          for(let i=0; i<db.length; i++) {
-            // 학습을 했을 경우 메시지 덮어쓰기
-            if(A == db[i][0]) {
-              db[i][1] = B;
-              db[i][2] = sender;
-
-              learned = true;
-              break;
-            }
-          }
-
-          // 학습을 안 했을 경우 메시지 추가
-          if(!learned)
-            db.push([A, B, sender]);
-
-          saveDB();
-          replier.reply("냥!");
-        }
-      } else if(query.length == 2) {
-        /**
-         * 명령어: 냥습/A
-         * 설명: 메시지 A를 학습시켰는지 확인한다.
-         */
-
-        let A = query[1], b = "", c = "";
-        let learned = false;
-
-        for(let i=0; i<db.length; i++) {
-          // 학습을 했는지 확인
-          if(A == db[i][0]) {
-            B = db[i][1];
-            C = db[i][2];
-
-            learned = true;
-            break;
-          }
-        }
-
-        if(learned)
-          replier.reply(B+", "+C+"님이 냥습시켯다냥!");
-        else
-          replier.reply("냥습한게 없다냥!");
-      }
+      if(query.length >= 3)
+        replier.reply(learn(query, sender)); // 학습시키기
+      else if(query.length == 2)
+        replier.reply(confirmLearn(query)); // 학습했는지 확인
     } else if(query[0] == "삭제") {
-      if(query.length >= 2) {
-        /**
-         * 명령어: 삭제/A
-         * 설명: 학습되어 있는 메시지 A를 삭제한다.
-         */
-
-        let A = query[1];
-        let learned = false;
-
-        for(let i=0; i<db.length; i++) {
-          // 학습되어 있으면 삭제
-          if(A == db[i][0]) {
-            db.splice(i, 1);
-
-            saveDB();
-            replier.reply("냥!");
-
-            learned = true;
-            break;
-          }
-        }
-
-        if(!learned)
-          replier.reply("냥습한게 없다냥!");
-      }
+      if(query.length >= 2)
+        replier.reply(del(query)); // 학습한거 삭제
     } else if(query[0] == "말") {
-      if(query.length >= 2) {
-        /**
-         * 명령어: 말/A
-         * 조건: A는 정수
-         * 설명: 이전에 왔던 메시지를 본다.
-         */
-
-        let A = Number(query[1]);
-
-        if(!Number.isInteger(A))
-          replier.reply("정수가 아니다냥!");
-        else if(!(0 <= A && msgList.length-(A+1) >= 0))
-          replier.reply("수가 범위를 초과했다냥!");
-        else
-          replier.reply(msgList[msgList.length-(A+1)][0]+", "+msgList[msgList.length-(A+1)][1]+"님이다냥!");
-      }
+      if(query.length >= 2)
+        replier.reply(prevMsg(query)); // 이전 메시지 보여주기
     } else if(query[0] == "화냥폰") {
-      if(query.length >= 2) {
-        /**
-         * 명령어: 화냥폰/A
-         * 설명: 화냥폰의 정보를 본다.
-         *   A ∈ { 배터리, 전압 }
-         */
-
-        let A = query[1];
-
-        if(A == "배터리")
-          replier.reply(Device.getBatteryLevel()+"% 남았다냥!");
-        else if(A == "전압")
-          replier.reply(Device.getBatteryVoltage()+"mV 이다냥!");
-      }
+      if(query.length >= 2)
+        replier.reply(phone(query)); // 화냥폰 정보 보여주기
     } else if(msg == "냥습목록") {
-      /**
-       * 명령어: 냥습목록
-       * 설명: 학습 목록을 본다.
-       */
-
-      let list = "< 냥습목록 >\n";
-
-      for(let i=0; i<db.length; i++)
-        list += String(i+1)+": "+String(db[i])+'\n';
-
-      replier.reply(list);
+      replier.reply(learnList()); // 학습목록 보여주기
     } else if(msg == "오늘은") {
-      /**
-       * 명령어: 오늘은
-       * 설명: 오늘 날짜를 본다.
-       */
-
-      let now = new Date();
-      replier.reply(now.getFullYear()+"년 "+(now.getMonth()+1)+"월 "+now.getDate()+"일이다냥!");
+      replier.reply(today()); // 오늘 날짜 보여주기
     } else if(msg == "안녕" || msg == "안녕하세요") {
-      /**
-       * 명령어: 안녕 or 안녕하세요
-       */
-
-      replier.reply(sender+"님 환영한다냥!");
+      replier.reply(hello(sender)); // 인사하기
     } else if(msg == "화냥봇") {
-      /**
-       * 명령어: 화냥봇
-       */
-
-      let ans = ["냥?", "냐앙?", "왜 불럿냥?", "무슨 일이냥?"];
-      let r = Math.floor(Math.random()*ans.length);
-      replier.reply(ans[r]);
+      replier.reply(nyanBot()); // 화냥봇 반응하기
     } else {
-      for(let i=0; i<db.length; i++) {
-        // 학습한거 활용
-        if(msg == db[i][0]) {
-          replier.reply(db[i][1]);
+      // 메시지가 오면 학습데이터에 따라 반응하기
+      for(let i=0; i<learnDB.length; i++) {
+        if(msg == learnDB[i][0]) {
+          replier.reply(learnDB[i][1]);
           break;
         }
       }
